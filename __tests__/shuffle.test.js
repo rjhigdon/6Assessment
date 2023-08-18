1a41679ca9b109a56b878c1c83d72f3bd2a4c7cb
rjhigdon <118936868+rjhigdon@users.noreply.github.com>
2023-08-18 12:38:15 -0800

    test 6.1

---
+++ __tests__/shuffle.test.js
@@ -0,0 +1,10 @@
+const shuffle = require("../src/shuffle");
+
+describe("shuffle should...", () => {
+  test("return an array", () =>{
+    expect(Array.isArray(shuffle())).toBe(true)
+  })
+  test("return an array of the same length as the argument sent in", () =>{
+    
+  })
+ });
---
+++ server.js
@@ -0,0 +1,96 @@
+const express = require("express");
+const path = require('path')
+const app = express();
+const bots = require("./src/botsData");
+const shuffle = require("./src/shuffle");
+
+app.use(express.static(path.join(`${__dirname}/public`)));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/index.html')) 
+})
+app.use(express.json());
+
+const playerRecord = {
+  wins: 0,
+  losses: 0,
+};
+
+// Add up the total health of all the robots
+const calculateTotalHealth = (robots) =>
+  robots.reduce((total, { health }) => total + health, 0);
+
+// Add up the total damage of all the attacks of all the robots
+const calculateTotalAttack = (robots) =>
+  robots
+    .map(({ attacks }) =>
+      attacks.reduce((total, { damage }) => total + damage, 0)
+    )
+    .reduce((total, damage) => total + damage, 0);
+
+// Calculate both players' health points after the attacks
+const calculateHealthAfterAttack = ({ playerDuo, compDuo }) => {
+  const compAttack = calculateTotalAttack(compDuo);
+  const playerHealth = calculateTotalHealth(playerDuo);
+  const playerAttack = calculateTotalAttack(playerDuo);
+  const compHealth = calculateTotalHealth(compDuo);
+
+  return {
+    compHealth: compHealth - playerAttack,
+    playerHealth: playerHealth - compAttack,
+  };
+};
+
+app.get("/api/robots", (req, res) => {
+  try {
+    res.status(200).send(bots);
+  } catch (error) {
+    console.error("ERROR GETTING BOTS", error);
+    res.sendStatus(400);
+  }
+});
+
+app.get("/api/robots/shuffled", (req, res) => {
+  try {
+    let shuffled = shuffle(bots);
+    res.status(200).send(shuffled);
+  } catch (error) {
+    console.error("ERROR GETTING SHUFFLED BOTS", error);
+    res.sendStatus(400);
+  }
+});
+
+app.post("/api/duel", (req, res) => {
+  try {
+    const { compDuo, playerDuo } = req.body;
+
+    const { compHealth, playerHealth } = calculateHealthAfterAttack({
+      compDuo,
+      playerDuo,
+    });
+
+    // comparing the total health to determine a winner
+    if (compHealth > playerHealth) {
+      playerRecord.losses += 1;
+      res.status(200).send("You lost!");
+    } else {
+      playerRecord.losses += 1;
+      res.status(200).send("You won!");
+    }
+  } catch (error) {
+    console.log("ERROR DUELING", error);
+    res.sendStatus(400);
+  }
+});
+
+app.get("/api/player", (req, res) => {
+  try {
+    res.status(200).send(playerRecord);
+  } catch (error) {
+    console.log("ERROR GETTING PLAYER STATS", error);
+    res.sendStatus(400);
+  }
+});
+
+app.listen(8000, () => {
+  console.log(`Listening on 8000`);
+});
