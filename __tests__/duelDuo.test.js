f4639dd9773cb591fe0455e1a0e2b09f4e887bf9
rjhigdon <118936868+rjhigdon@users.noreply.github.com>
2023-08-24 18:24:44 -0800

    test take 2

--- __tests__/duelDuo.test.js
+++ __tests__/duelDuo.test.js
@@ -11,16 +11,30 @@ afterEach(async () => {
 });
 
 describe("Duel Duo tests", () => {
-  test("page loads with title", async () => {
+  // test("page loads with title", async () => {
+  //   await driver.get("http://localhost:8000");
+  //   await driver.wait(until.titleIs("Duel Duo"), 1000);
+  // });
+  // test("clicking 'Draw' displays bot choices", async () =>{
+  //   await driver.get("http://localhost:8000");
+  //   await driver.wait(until.titleIs("Duel Duo"), 1000);
+  //   await driver.sleep(500)
+  //   await driver.findElement(By.id('draw')).click()
+  //   await driver.sleep(500)
+  //   const isPresent = await driver.findElement(By.id("choices")).isDisplayed()
+  //   expect(isPresent).toBeTruthy()
+  // });
+
+  test("clicking 'Add to Duo' displays bot choices", async () =>{
     await driver.get("http://localhost:8000");
     await driver.wait(until.titleIs("Duel Duo"), 1000);
-  });
-  test("clicking 'Draw' displays bot choices", async () =>{
-    await driver.get("http://localhost:8000");
-    await driver.wait(until.titleIs("Duel Duo"), 1000);
-    await driver.sleep(2000)
+    await driver.sleep(500)
     await driver.findElement(By.id('draw')).click()
-    await driver.sleep(2000)
-    expect 
+    await driver.sleep(1000)
+    await driver.findElement(By.class('bot-btn')).click()
+    await driver.sleep(1000)
+    const isPresent = await driver.findElement(By.id("player-duo")).isDisplayed()
+    await driver.sleep(1000)
+    expect(isPresent).toBeTruthy()
   });
 });
